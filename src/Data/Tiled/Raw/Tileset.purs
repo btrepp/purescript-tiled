07aa0be529c37ea920720fd97b181fa05f8c6080
beau trepp <beautrepp@gmail.com>
2018-11-06 17:53:09 +0800

    Tileset can be parsed in json form
    This correctly parses the example tiled tileset
    saved as json (and vendored) into
    the tileset record

--- src/Data/Tiled/Raw/Tileset.purs
+++ src/Data/Tiled/Raw/Tileset.purs
@@ -1,13 +1,62 @@
 module Data.Tiled.Raw.Tileset where
   
-import Data.Argonaut.Core(Json)
-import Data.Either (Either(..))
-data Tileset = Tileset {
-    columns :: Int
-}
+import Prelude
 
-parseTileSet :: Json -> Either String Tileset 
-parseTileSet json = 
-    Right tiles 
-    where 
-        tiles = Tileset { columns : 2 }
+import Data.Argonaut (class DecodeJson, decodeJson, (.?))
+
+
+newtype Terrain = Terrain 
+    { name :: String
+      , tile :: Int
+    }
+instance eqTerrain :: Eq Terrain where
+    eq (Terrain a) (Terrain b ) = a == b
+instance showTerrain :: Show Terrain where
+    show (Terrain x) = show x
+    
+type Version = String 
+
+newtype Tileset = Tileset 
+    { columns :: Int
+      , image :: String
+      , imageHeight :: Int
+      , imageWidth :: Int
+      , margin :: Int
+      , name :: String
+      , spacing :: Int
+      , terrains :: Array Terrain
+      , tileCount :: Int
+      , tiledVersion :: Version
+      , tileHeight :: Int
+    }
+
+instance showTileset :: Show Tileset where
+    show (Tileset t) = show t
+instance eqTileset :: Eq Tileset where
+    eq (Tileset t) (Tileset t2) = t == t2
+instance decodeTileSet :: DecodeJson Tileset where
+  decodeJson json = do
+    obj <- decodeJson json
+    columns <- obj .? "columns"
+    image <- obj .? "image"
+    imageHeight <- obj .? "imageheight"
+    imageWidth <- obj .? "imagewidth"
+    margin <- obj .? "margin"
+    name <- obj .? "name"
+    spacing <- obj .? "spacing"
+    tileCount <- obj .? "tilecount"
+    tiledVersion <- obj .? "tiledversion"
+    tileHeight <- obj .? "tileheight"
+    terrains <- pure mempty
+
+    pure $ Tileset {   columns
+                     , image
+                     , imageHeight
+                     , imageWidth
+                     , margin
+                     , name
+                     , spacing 
+                     , terrains
+                     , tileCount
+                     , tiledVersion
+                     , tileHeight }
--- test/TilesetSpec.purs
+++ test/TilesetSpec.purs
@@ -1,34 +1,62 @@
-module Data.Tiled.Raw.Tileset.Unit where
+module Data.Tiled.Raw.Tileset.Unit (tilesetSuite) where
 
 import Prelude
 
-import Data.Argonaut (Json)
+import Data.Argonaut (decodeJson)
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either(..))
-import Data.Tiled.Raw.Tileset (Tileset, parseTileSet)
-import Data.Tiled.Raw.Tileset as Tileset
+import Data.Tiled.Raw.Tileset (Tileset(..))
 import Effect.Aff (Aff, error, throwError)
-import Test.Unit ( TestSuite, suite, test)
+import Effect.Class (liftEffect)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync as FS
+import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
 
-jsonText :: String 
-jsonText = "{\"columns\":2}"
+loadTileSet :: String -> Aff Tileset
+loadTileSet path = do
+  file <- liftEffect $ FS.readTextFile ASCII path
+  js <- json file
+  case decodeJson js of
+    Left x -> throwError (error x)
+    Right x -> pure x
+  where json text =
+          case jsonParser text of
+          Left x -> throwError (error x)
+          Right x -> pure x
 
-json :: Either String Json
-json = jsonParser jsonText
+desertSuite :: TestSuite
+desertSuite = 
+  suite "desert" do
+    test "parsed" do
+      actual >>= Assert.equal expected
 
-tileset :: Aff Tileset
-tileset = 
-  case json of
-  Left x -> throwError (error x)
-  Right y -> case parseTileSet y of
-             Left x -> throwError (error x)
-             Right z -> pure z
+  where actual =  loadTileSet "maps/desert_tileset.json"
+        columns = 8
+        image = "tmw_desert_spacing.png"
+        imageHeight = 199
+        imageWidth = 265
+        margin = 1
+        name = "Desert"
+        spacing = 1
+        terrains = []
+        tileCount = 48
+        tiledVersion = "1.2.0"
+        tileHeight = 32
+        expected = Tileset {   
+                     columns
+                     , image
+                     , imageHeight
+                     , imageWidth
+                     , margin
+                     , name
+                     , spacing 
+                     , terrains
+                     , tileCount
+                     , tiledVersion
+                     , tileHeight }
 
 tilesetSuite :: TestSuite
 tilesetSuite  =  
-  suite "tileset" do
-    test "has correct columns" do
-      (Tileset.Tileset tiles) <- tileset
-      Assert.equal tiles.columns 2
-
+  suite "Tileset" do
+    desertSuite
